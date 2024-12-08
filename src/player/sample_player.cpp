469a8729e338a1ba794218f7c0771c608aad6e1f
davi <davi.muniz41@gmail.com>
2024-12-08 15:14:33 -0300

    refactor: file name

--- src/player/sample_player.cpp
+++ src/player/sample_player.cpp
@@ -248,7 +248,7 @@ SamplePlayer::actionImpl()
 
     static std::ofstream custom_log;
     if (!custom_log.is_open()) {
-        std::string filename = "./positions";
+        std::string filename = "./game_mode_";
         filename += std::to_string(world().self().unum());
         filename += ".csv";
 
